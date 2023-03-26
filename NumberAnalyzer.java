69ae3ebbbb23df9f00dd5c6eda5dc463e0397390
diffloc <6632742+diffloc@users.noreply.github.com>
2023-03-25 22:30:53 -0500

    Stage 4/8

--- NumberAnalyzer.java
+++ NumberAnalyzer.java
@@ -1,5 +1,7 @@
 package numbers;
 
+import java.util.ArrayList;
+
 public class NumberAnalyzer {
 
     private NumberAnalyzer() {
@@ -14,6 +16,7 @@ public class NumberAnalyzer {
     }
 
     private static boolean isDuckNumber(long number) {
+
         return String.valueOf(number).indexOf("0") > 0;
     }
 
@@ -21,13 +24,59 @@ public class NumberAnalyzer {
         return String.valueOf(number).equals(new StringBuilder(String.valueOf(number)).reverse().toString());
     }
 
-    public static void numberProperties(long number) {
+    public static boolean isGapful(long number) {
+        String numString = Long.toString(number);
+        if (numString.length() < 3) {
+            return false;
+        }
+        String firstDigit = numString.substring(0, 1);
+        String lastDigit = numString.substring(numString.length() - 1);
+        int gapConcat = Integer.parseInt(firstDigit + lastDigit);
+        return number % gapConcat == 0;
+    }
+
+    public static void numberProperties(long number, int param) {
+        if (param == 0) {
+            singleNumberProps(number);
+        } else {
+            rangeNumberProps(number, param);
+        }
+    }
+
+    public static void singleNumberProps(long number) {
         System.out.println("\nProperties of " + number);
-        System.out.println("\t\teven: " + isEven(number));
-        System.out.println("\t\t odd: " + !isEven(number));
         System.out.println("\t\tbuzz: " + isBuzzNumber(number));
         System.out.println("\t\tduck: " + isDuckNumber(number));
         System.out.println(" palindromic: " + isPalindromic(number));
+        System.out.println("\t  gapful: " + isGapful(number));
+        System.out.println("\t\teven: " + isEven(number));
+        System.out.println("\t\t odd: " + !isEven(number));
     }
 
+    public static void rangeNumberProps(long number, int param) {
+        ArrayList<String> numberTests;
+        for (int i = 0; i < param; i++) {
+            numberTests = new ArrayList<>();
+            long curNumber = number + i;
+            if (isBuzzNumber(curNumber)) {
+                numberTests.add("buzz");
+            }
+            if (isDuckNumber(curNumber)) {
+                numberTests.add("duck");
+            }
+            if (isPalindromic(curNumber)) {
+                numberTests.add("palindromic");
+            }
+            if (isGapful(curNumber)) {
+                numberTests.add("gapful");
+            }
+            if (isEven(curNumber)) {
+                numberTests.add("even");
+            }
+            if (!isEven(curNumber)) {
+                numberTests.add("odd");
+            }
+            System.out.println("\t\t\t " +curNumber + " is " + String.join(", ", numberTests));
+        }
+    }
 }
--- UserInterface.java
+++ UserInterface.java
@@ -6,40 +6,71 @@ import java.util.Scanner;
 public class UserInterface {
 
     private final Scanner uiScan;
-    static final String BEGIN_MSG = ("Welcome to Amazing Numbers!\n\n"
-            + "Supported requests:\n"
+    static final String BEGIN_MSG = ("Welcome to Amazing Numbers!");
+    static final String INSTRUCTIONS = ("\nSupported requests:\n"
             + "- enter a natural number to know its properties;\n"
+            + "- enter two natural numbers to obtain the properties of the list:\n"
+            + "  * the first parameter represents a starting number;\n"
+            + "  * the second parameter shows how many consecutive numbers are to be processed;\n"
+            + "- separate the parameters with one space;\n"
             + "- enter 0 to exit.");
     static final String END_MSG = ("\nGoodbye!");
 
     static final String PROMPT = ("\nEnter a request: ");
-    static final String INVALID_INPUT = ("The first parameter should be a natural number or zero.");
+    static final String INVALID_FIRST = ("\nThe first parameter should be a natural number or zero.");
+    static final String INVALID_SECOND = ("\nThe second parameter should be a natural number.");
 
     public UserInterface() {
+
         this.uiScan = new Scanner(System.in);
     }
 
     public void run() {
         System.out.println(BEGIN_MSG);
+        System.out.println(INSTRUCTIONS);
 
         while (true) {
             System.out.print(PROMPT);
-            long userInput;
+            String userInput = uiScan.nextLine();
+            if (userInput.isEmpty()) {
+                System.out.println(INSTRUCTIONS);
+                continue;
+            }
+
+            String[] parts = userInput.split(" ");
+            long userNumber;
+            int userParam = 0;
+
             try {
-                userInput = uiScan.nextLong();
-                if (userInput == 0) {
+                userNumber = Long.parseLong(parts[0]);
+                if (userNumber == 0) {
                     System.out.println(END_MSG);
                     break;
                 }
-                if (userInput < 0) {
-                    System.out.println(INVALID_INPUT);
+                if (userNumber < 0) {
+                    System.out.println(INVALID_FIRST);
                     continue;
                 }
-            } catch (InputMismatchException e) {
-                System.out.println("Input Mismatch Exception: " + uiScan.next() + " is not a natural number!");
+
+                try {
+                    if (parts.length > 1) {
+                        userParam = Integer.parseInt(parts[1]);
+                    }
+                    if (userParam < 0) {
+                        System.out.println(INVALID_SECOND);
+                        continue;
+                    }
+
+                } catch (NumberFormatException e) {
+                    System.out.println(INVALID_SECOND);
+                    continue;
+                }
+
+            } catch (NumberFormatException e) {
+                System.out.println(INVALID_FIRST);
                 continue;
             }
-            NumberAnalyzer.numberProperties(userInput);
+            NumberAnalyzer.numberProperties(userNumber, userParam);
         }
 
     }
